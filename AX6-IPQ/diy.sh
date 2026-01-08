55fd7c72dc81886a1d863dedf46082bcba663a6c
ydhang2024 <157379950+ydhang2024@users.noreply.github.com>
2026-01-08 22:05:32 +0800

    Update diy.sh

--- AX6-IPQ/diy.sh
+++ AX6-IPQ/diy.sh
@@ -60,7 +60,7 @@ rm -rf feeds/luci/applications/openwrt-passwall
 
 
 #修改默认IP
-sed -i 's/192.168.1.1/192.168.10.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 #修改主机名
 sed -i "s/hostname='ImmortalWrt'/hostname='Redmi-AX6'/g" package/base-files/files/bin/config_generate
